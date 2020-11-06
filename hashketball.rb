314c880339129d5508e7d13f581cf1f30c27eeef
Conor Murnane <conormurnane11@gmail.com>
2020-11-06 22:16:27 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -69,13 +69,15 @@ def game_hash
         :player_name => "Jeff Adrien",
         :number => 4,
         :shoe => 18,
-        :points => "",
-        :rebounds => "",
-        :assists => "",
-        :steals => "",
-        :blocks => "",
-        :slam_dunks => ""
+        :points => 10,
+        :rebounds => 1,
+        :assists => 1,
+        :steals => 2,
+        :blocks => 7,
+        :slam_dunks => 2
         }
+        
+        
         ]
     }
     
