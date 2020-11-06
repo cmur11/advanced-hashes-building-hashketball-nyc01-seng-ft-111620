ebb36119001b551fc32f326d6a5e125f6ee3c21a
Conor Murnane <conormurnane11@gmail.com>
2020-11-06 22:07:24 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -7,13 +7,13 @@ def game_hash
       :colors => ["Black", "White"],
       :players => [
         {
-        :player_name => "Alan Anderson","Reggie Evans", "Brook Lopez", "Mason Plumlee", "Jason Terry"
-        :number => ["0","30","11","1","31"],
-        :shoe => ["16","14","17","19","15"],
-        :points => [22,]
-        :rebounts => "",
-        :assists => "",
-        :blocks => "",
+        :player_name => "Alan Anderson",
+        :number => "0",
+        :shoe => "16",
+        :points => "22",
+        :rebounts => "12",
+        :assists => "12",
+        :blocks => "1",
         :slam_dunks => ""
         }      
         ]
