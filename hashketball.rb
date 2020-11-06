ed29aa5b0e5c1eb4e8693fa8040f120260d159fb
Conor Murnane <conormurnane11@gmail.com>
2020-11-06 22:19:09 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -100,14 +100,14 @@ def game_hash
         },
                     {
         :player_name => "Ben Gordon",
-        :number => 2,
-        :shoe => 14,
-        :points => 24,
-        :rebounds => 12,
-        :assists => 12,
-        :steals => 4,
-        :blocks => 5,
-        :slam_dunks => 5
+        :number => 8,
+        :shoe => 15,
+        :points => 33,
+        :rebounds => 3,
+        :assists => 2,
+        :steals => 1,
+        :blocks => 1,
+        :slam_dunks => 0
         },
         
         
